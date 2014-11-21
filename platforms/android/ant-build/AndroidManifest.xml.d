9cebf1bff4999f5ceca716c6428cd332f520e668
Daniel <xamctep4e@gmail.com>
2014-11-21 14:47:03 +0200

    initial commit

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\AndroidManifest.xml : \
+D:\ionic-calendar\ionic-calendar\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,13 @@
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes.dex : \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionic\keyboard\IonicKeyboard$1.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionic\keyboard\IonicKeyboard$2.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionic\keyboard\IonicKeyboard.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionicframework\calendar759432\BuildConfig.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionicframework\calendar759432\CordovaApp.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionicframework\calendar759432\R$attr.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionicframework\calendar759432\R$drawable.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionicframework\calendar759432\R$string.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionicframework\calendar759432\R$xml.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\com\ionicframework\calendar759432\R.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\classes\org\apache\cordova\device\Device.class \
+D:\ionic-calendar\ionic-calendar\platforms\android\ant-build\dexedLibs\classes-ff7507252d45fa5e2a7f0af596386cab.jar \
