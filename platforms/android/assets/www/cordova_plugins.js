bf9e32ed9677bd38a082e24fde08c2c8539a04cd
Kiril Kirov <kkirov@web-teh.net>
2014-11-27 23:15:11 +0200

    added calendar plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -13,6 +13,17 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.calendar/www/Calendar.js",
+        "id": "nl.x-services.plugins.calendar.Calendar",
+        "clobbers": [
+            "Calendar"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.calendar/test/tests.js",
+        "id": "nl.x-services.plugins.calendar.tests"
     }
 ];
 module.exports.metadata = 
@@ -20,7 +31,8 @@ module.exports.metadata =
 {
     "com.ionic.keyboard": "1.0.3",
     "org.apache.cordova.console": "0.2.11",
-    "org.apache.cordova.device": "0.2.12"
+    "org.apache.cordova.device": "0.2.12",
+    "nl.x-services.plugins.calendar": "4.2.8"
 }
 // BOTTOM OF METADATA
 });
