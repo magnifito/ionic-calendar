4699e98435259f55359cb5d9f0173094bab7fcd3
Kiril Kirov <kkirov@web-teh.net>
2014-12-09 10:58:38 +0200

    updated popup content

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -3,8 +3,8 @@
 
 var app = angular.module('ionic-calendar', ['ionic'])
 
-.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -17,10 +17,10 @@ var app = angular.module('ionic-calendar', ['ionic'])
     });
 })
 
-.config(function ($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
 
-    .state('app', {
+        .state('app', {
         url: "/app",
         abstract: true,
         templateUrl: "templates/menu.html",
@@ -37,13 +37,13 @@ var app = angular.module('ionic-calendar', ['ionic'])
     })
 
     .state('app.browse', {
-        url: "/browse",
-        views: {
-            'menuContent': {
-                templateUrl: "templates/browse.html"
+            url: "/browse",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/browse.html"
+                }
             }
-        }
-    })
+        })
         .state('app.demo2', {
             url: "/demo2",
             views: {
@@ -66,38 +66,38 @@ var app = angular.module('ionic-calendar', ['ionic'])
     $urlRouterProvider.otherwise('/app/demo1');
 });
 
-app.controller("MyController", function ($scope, $http) {
+app.controller("MyController", function($scope, $http) {
     $http.get('js/events.txt').
-    success(function (data, status, headers, config) {
+    success(function(data, status, headers, config) {
         $scope.events = data;
 
 
     }).
-    error(function (data, status, headers, config) {
+    error(function(data, status, headers, config) {
         // log error
     });
 });
 
-app.controller('MyController', function ($scope, $ionicPopup, $timeout) {
+app.controller('CalendarController', function($scope, $ionicPopup, $timeout) {
 
     // Triggered on a button click, or some other target
-    $scope.showPopup = function () {
+    $scope.showPopup = function($event, day) {
         $scope.data = {}
+        if (day.events.length > 0) {
+        //show popup only if events on day cell
+        
+        var $element = $event.currentTarget;
 
-        // An elaborate, custom popup
-        var myPopup = $ionicPopup.show({
-            template: '<input type="password" ng-model="data.wifi">',
-            title: 'Enter Wi-Fi Password',
-            subTitle: 'Please use normal things',
-            scope: $scope,
-            buttons: [
-                {
-                    text: 'Cancel'
-                },
-                {
-                    text: '<b>Save</b>',
+            // An elaborate, custom popup
+            var myPopup = $ionicPopup.show({
+                templateUrl: '/templates/calendar/calendar-event-popup-template.html',
+                title: '' + day.date,
+                subTitle: '',
+                scope: $scope,
+                buttons: [{
+                    text: '<b>Close</b>',
                     type: 'button-positive',
-                    onTap: function (e) {
+                    onTap: function(e) {
                         if (!$scope.data.wifi) {
                             //don't allow the user to close unless he enters wifi password
                             e.preventDefault();
@@ -105,15 +105,19 @@ app.controller('MyController', function ($scope, $ionicPopup, $timeout) {
                             return $scope.data.wifi;
                         }
                     }
-      },
-    ]
-        });
-        myPopup.then(function (res) {
+                }, ]
+            });
+        myPopup.then(function(res) {
             console.log('Tapped!', res);
         });
-        $timeout(function () {
+        $timeout(function() {
             myPopup.close(); //close the popup after 3 seconds for some reason
         }, 3000);
+        }
+
+
+
+       
     };
 });
 
@@ -146,33 +150,136 @@ var language = {
 
 };
 
+/**
+ * Language settings
+ *
+ * @param lang
+ * @returns {{month_labels: Array, dow_labels: Array}}
+ */
+var calendar_language = function(lang) {
+    if (typeof(lang) == 'undefined' || lang === false) {
+        lang = 'en';
+    }
+
+    switch (lang.toLowerCase()) {
+        case 'de':
+            return {
+                month_labels: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+                dow_labels: ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+            };
+            break;
+
+        case 'en':
+            return {
+                month_labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                dow_labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+            };
+            break;
+
+        case 'ar':
+            return {
+                month_labels: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
+                dow_labels: ["أثنين", "ثلاثاء", "اربعاء", "خميس", "جمعه", "سبت", "أحد"]
+            };
+            break;
+
+        case 'es':
+            return {
+                month_labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+                dow_labels: ["Lu", "Ma", "Mi", "Ju", "Vi", "Sá", "Do"]
+            };
+            break;
+
+        case 'fr':
+            return {
+                month_labels: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+                dow_labels: ["Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"]
+            };
+            break;
+
+        case 'it':
+            return {
+                month_labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+                dow_labels: ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"]
+            };
+            break;
+
+        case 'nl':
+            return {
+                month_labels: ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"],
+                dow_labels: ["Ma", "Di", "Wo", "Do", "Vr", "Za", "Zo"]
+            };
+            break;
+
+        case 'pt':
+            return {
+                month_labels: ["Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+                dow_labels: ["S", "T", "Q", "Q", "S", "S", "D"]
+            };
+            break;
+
+        case 'ru':
+            return {
+                month_labels: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+                dow_labels: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вск"]
+            };
+            break;
+
+        case 'se':
+            return {
+                month_labels: ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"],
+                dow_labels: ["Mån", "Tis", "Ons", "Tor", "Fre", "Lör", "Sön"]
+            };
+            break;
+
+        case 'bg':
+            return {
+                month_labels: ["Януари", "Февруари", "Март", "Април", "Май", "Юни", "Юли", "Август", "Септември", "Октомври", "Ноември", "Декември"],
+                dow_labels: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нед"]
+            };
+            break;
+
+        case 'tr':
+            return {
+                month_labels: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+                dow_labels: ["Pts", "Salı", "Çar", "Per", "Cuma", "Cts", "Paz"]
+            };
+            break;
+    }
+
+};
+
 var monthNames = ["Януари", "Февруари", "Март", "Април", "Май", "Юни",
-    "Юли", "Август", "Септември", "Октомври", "Ноември", "Декември"];
+    "Юли", "Август", "Септември", "Октомври", "Ноември", "Декември"
+];
+
+
+
 
 // get month name
-Date.prototype.getMonthName = function () {
+Date.prototype.getMonthName = function() {
     var currentDate = new Date();
     var month = monthNames[this.getMonth()];
     return month < 10 ? '0' + month : month;
 
 }
 
-Date.prototype.getMonthFormatted = function () {
+Date.prototype.getMonthFormatted = function() {
     var month = this.getMonth() + 1;
     return month < 10 ? '0' + month : month;
 }
 
 
-app.directive('ngHtml', function () {
-    return function (scope, element, attrs) {
-        scope.$watch(attrs.ngHtml, function (value) {
+app.directive('ngHtml', function() {
+    return function(scope, element, attrs) {
+        scope.$watch(attrs.ngHtml, function(value) {
             element[0].innerHTML = value;
         });
     }
 });
 
 
-var calendarLinkFunction = function (scope, element) {
+var calendarLinkFunction = function(scope, element) {
     var contentObj = scope.content;
     var targetMonth = parseInt(scope.assignedMonth, 10),
         targetYear = parseInt(scope.assignedyear, 10);
@@ -192,25 +299,25 @@ var calendarLinkFunction = function (scope, element) {
     scope.navigate = {};
 
     // month between 1 and 12
-    var daysInMonth = function (month, year) {
+    var daysInMonth = function(month, year) {
         return new Date(year, month, 0).getDate();
     }
 
-    scope.navigate.prevMotnth = function () {
+    scope.navigate.prevMotnth = function() {
         scope.currentDate.setMonth(scope.currentDate.getMonth() - 1);
         refreshCalendar();
     }
-    scope.navigate.nextMotnth = function () {
+    scope.navigate.nextMotnth = function() {
         scope.currentDate.setMonth(scope.currentDate.getMonth() + 1);
         refreshCalendar();
     }
-    scope.navigate.thisMotnth = function () {
+    scope.navigate.thisMotnth = function() {
         scope.currentDate = new Date();
         refreshCalendar();
     }
 
     // month between 1 ~ 12
-    var getDateContent = function (year, month, date) {
+    var getDateContent = function(year, month, date) {
         if (contentObj != null && contentObj[year] != null &&
             contentObj[year][month] != null &&
             contentObj[year][month][date] != null) {
@@ -220,7 +327,7 @@ var calendarLinkFunction = function (scope, element) {
     }
 
     // month between 1 ~ 12
-    var monthGenegrator = function (month, year, events) {
+    var monthGenegrator = function(month, year, events) {
         var monthArray = [];
         var firstDay = new Date(year, month - 1, 1, 0, 0, 0, 0);
         //  weekDay between 1 ~ 7 , 1 is Monday, 7 is Sunday
@@ -252,7 +359,7 @@ var calendarLinkFunction = function (scope, element) {
     }
 
     //month between 1~12
-    var weekGenegrator = function (year, month, startDate, daysOfMonth, prevDaysOfMonth, events) {
+    var weekGenegrator = function(year, month, startDate, daysOfMonth, prevDaysOfMonth, events) {
         var week = [];
 
         for (var i = 1; i <= 7; i++) {
@@ -260,32 +367,28 @@ var calendarLinkFunction = function (scope, element) {
                 realDate,
                 outmonth = false,
                 content = "";
-            var events = [
-                {
+            var events = [{
                     "id": "1",
                     "title": "Example 1",
                     "url": "http://www.example.com/",
                     "description": "This is event description",
                     "start": "2014-12-1",
                     "end": "2014-12-04"
-                },
-                {
+                }, {
                     "id": "2",
                     "title": "Example 2",
                     "url": "http://www.example.com/",
                     "description": "This is event description",
                     "start": "2014-12-2",
                     "end": "2014-12-04"
-                },
-                {
+                }, {
                     "id": "3",
                     "title": "Example 3",
                     "url": "http://www.example.com/",
                     "description": "This is event description",
                     "start": "2014-12-5",
                     "end": "2014-12-04"
-                },
-                {
+                }, {
                     "id": "4",
                     "title": "Example 4 ",
                     "url": "http://www.example.com/",
@@ -316,7 +419,7 @@ var calendarLinkFunction = function (scope, element) {
             for (var m = 0; m < events.length; m++) {
                 if (events[m].start === fullDate && !outmonth) {
                     dayEvents.push(events[m].title);
-                }   
+                }
             }
 
             week.push({
@@ -333,7 +436,7 @@ var calendarLinkFunction = function (scope, element) {
         return week;
     }
 
-    var refreshCalendar = function (events) {
+    var refreshCalendar = function(events) {
         scope.month = monthGenegrator(scope.currentDate.getMonth() + 1, scope.currentDate.getFullYear(), events);
     }
 
@@ -341,7 +444,7 @@ var calendarLinkFunction = function (scope, element) {
 }
 
 
-app.directive("calendar", function () {
+app.directive("calendar", function() {
     return {
         restrict: "EA",
         scope: {
